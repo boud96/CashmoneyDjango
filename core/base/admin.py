3555e0880547616e11063fb9cbbac01c29623cf8
Adam.Boudnik <adam.boudnik@olc.cz>
2024-11-09 13:02:09 +0100

    first models

--- core/base/admin.py
+++ core/base/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.base.models import BankAccount, Category, Transaction, Subcategory
+
+
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = (
+        "account_name",
+        "account_number",
+        "bank_code",
+    )
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "date_of_transaction",
+        "amount",
+        "currency",
+        "counterparty_name",
+        "counterparty_note",
+        "my_note",
+        "other_note"
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description"
+    )
+
+
+@admin.register(Subcategory)
+class Subcategory(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "category"
+    )
--- core/base/models.py
+++ core/base/models.py
@@ -59,3 +59,55 @@ class ImageAnnotationAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     )
+
+
+class BankAccount(AbstractBaseModel):
+    account_number = models.CharField(max_length=128, blank=False, null=False)
+    bank_code = models.CharField(max_length=128, null=True, blank=True)
+    account_name = models.CharField(max_length=128, null=False, blank=False)
+
+    def __str__(self):
+        return str(self.account_name)
+
+
+class Transaction(AbstractBaseModel):
+    date_of_submission = models.DateTimeField(null=True, blank=True)
+    date_of_transaction = models.DateTimeField(null=False, blank=False)
+
+    counterparty_account_number = models.CharField(max_length=128, null=True, blank=True)
+    counterparty_bank_code = models.CharField(max_length=128, null=True, blank=True)
+    counterparty_name = models.CharField(max_length=128, null=True, blank=True)
+
+    transaction_type = models.CharField(max_length=128, null=True, blank=True)
+    variable_symbol = models.CharField(max_length=128, null=True, blank=True)
+    specific_symbol = models.CharField(max_length=128, null=True, blank=True)
+    constant_symbol = models.CharField(max_length=128, null=True, blank=True)
+
+    counterparty_note = models.TextField(null=True, blank=True)
+    my_note = models.TextField(null=True, blank=True)
+    other_note = models.TextField(null=True, blank=True)
+
+    amount = models.DecimalField(max_digits=12, decimal_places=2, null=False, blank=False)
+    currency = models.CharField(max_length=3, null=False, blank=False)
+
+    def __str__(self):
+        counterparty = self.counterparty_name if self.counterparty_name else ""
+        return f"{self.date_of_transaction} - {self.currency}{self.amount} - {counterparty}"
+
+
+class Category(AbstractBaseModel):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Subcategory(AbstractBaseModel):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.category})"
