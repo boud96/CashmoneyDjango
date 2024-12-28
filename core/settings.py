ee8dbfca346ca510d77bebbbd02ae77958c88749
Adam.Boudnik <adam.boudnik@olc.cz>
2024-12-28 08:02:16 +0100

    switch to postgres

--- core/settings.py
+++ core/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES = {  # TODO: Use .env file
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",  # Or 'django.db.backends.mysql'
+        "NAME": "cashmoney_django_dev",
+        "USER": "postgres",
+        "PASSWORD": "admin",
+        "HOST": "localhost",  # Or your database server
+        "PORT": "5432",       # Default PostgreSQL port (or 3306 for MySQL)
     }
 }
 
