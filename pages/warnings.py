c68a278b75cc7e27b2ab9a22a662ab37e1ca2c3e
Adam.Boudnik <adam.boudnik@olc.cz>
2024-12-31 09:14:41 +0100

    warnings page WIP

---
+++ pages/warnings.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.header("TODO: Warnings like")
+st.write("Detected duplicates by same original ID")
+st.write("Uncategorized transactions")
+st.write("Mark warned transaction as OK")
