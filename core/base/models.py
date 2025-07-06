e7b1597cb2f1f458b0ea80a791ae1268e2e6f5c4
Adam.Boudnik <adam.boudnik@olc.cz>
2025-07-06 14:50:54 +0200

    handle None case in `get_default_bank_account` to prevent potential AttributeError

--- core/base/models.py
+++ core/base/models.py
@@ -256,7 +256,10 @@ class Keyword(AbstractBaseModel):
 
 
 def get_default_bank_account():
-    return BankAccount.objects.first().id  # Or another way to get a valid instance
+    bank_account = BankAccount.objects.first()
+    if bank_account:
+        return bank_account.id
+    return None
 
 
 class CSVMapping(AbstractBaseModel):
