1e8171ebb63ad01076660c044708922ae9b37411
Adam Boudník <adam.boudnik@aiviro.com>
2025-07-23 19:39:40 +0200

    Configure static file settings in Django project

--- core/settings.py
+++ core/settings.py
@@ -124,6 +124,10 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Add this line
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
