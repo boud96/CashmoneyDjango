6dc90d6a1fa8087115dbbc4b3e2817692634c0a5
Adam Boudník <boudnikadam@gmail.com>
2025-11-29 18:57:12 +0100

    Expand constants with new keyword routes and introduce `ModelConstants` for WNI choices and rule keys.

--- constants.py
+++ constants.py
@@ -2,4 +2,15 @@ class URLConstants:
     ADMIN = "admin/"
     IMPORT_TRANSACTIONS = "import-transactions/"
     RECATEGORIZE_TRANSACTIONS = "recategorize-transactions/"
-    UPDATE_KEYWORDS = "update-keywords/"
+    CREATE_KEYWORDS = "create-keywords/"
+    DELETE_KEYWORDS = "delete-keywords/"
+
+class ModelConstants:
+    WNI_CHOICES = [
+        ("want", "Want"),
+        ("investment", "Investment"),
+        ("need", "Need"),
+        ("other", "Other"),
+]
+    INCLUDE_RULE_KEY = "include"
+    EXCLUDE_RULE_KEY = "exclude"
